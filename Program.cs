74d8a2677e6d12f329749a5b9d8ec559f27f4c18
baileydiaz <baileydiaz10@gmail.com>
2022-11-29 13:41:49 -0700

    created inital screen

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Raylib_cs;
+using System.Numerics;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+  
+
+            Raylib.InitWindow(ScreenWidth, ScreenHeight, "GameObject");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+               
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+                
+
+                Raylib.EndDrawing();
+
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
+
