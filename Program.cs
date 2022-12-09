388b610f0004ac0009e7fe842d893c5cf317734d
baileydiaz <baileydiaz10@gmail.com>
2022-12-09 13:28:57 -0700

    hello

--- Program.cs
+++ Program.cs
@@ -9,20 +9,24 @@ using System.Numerics;
             var MovementSpeed = 4;
             var Herorec = new Rectangle(10, 460, 20, 20);
             var Vilrec = new Rectangle(750, 50 , 20, 20);
-            var WallRec = new Rectangle(600, 400 , 400, 20);
-            var WallRec2 = new Rectangle(200, 0 , 20, 400);
-            var WallRec3 = new Rectangle(500 ,0 , 20, 400);
+
+            var WallRec = new Rectangle(0, 100 , 450, 20);
+            var WallRec2 = new Rectangle(0, 400 , 250, 20);
+            var WallRec3 = new Rectangle(400 ,150 , 20, 400);
+            var WallRec4 = new Rectangle(700 ,10 , 20, 400);
             
             Hero hero = new Hero(MovementSpeed, Herorec);
             Villain villain = new Villain(Vilrec);
             wall wallrec1 = new wall(WallRec);
             wall wallrec2 = new wall(WallRec2);
             wall wallrec3 = new wall(WallRec3);
+            wall wallrec4 = new wall(WallRec4);
 
             thing thing = new thing(); 
             thing.Things.Add(wallrec1);
             thing.Things.Add(wallrec2);
             thing.Things.Add(wallrec3);
+            thing.Things.Add(wallrec4);
             List<thing> things = thing.Things;
 
             Raylib.InitWindow(ScreenWidth, ScreenHeight, "GameObject");
@@ -36,6 +40,7 @@ using System.Numerics;
                 wallrec1.drawWall();
                 wallrec2.drawWall();
                 wallrec3.drawWall();
+                wallrec4.drawWall();
                 villain.DrawVillain();
                 hero.input();
                 Raylib.EndDrawing();
